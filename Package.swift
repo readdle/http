682b180e4104aef2387c24747a38bf033efa3708
Albert Aleksieiev <albert.aleksieiev@gmail.com>
2018-09-05 12:55:51 +0300

    update Package.swift

--- Package.swift
+++ Package.swift
@@ -8,13 +8,13 @@ let package = Package(
     ],
     dependencies: [
         // 🌎 Utility package containing tools for byte manipulation, Codable, OS APIs, and debugging.
-        .package(url: "https://github.com/readdle/vapor-core.git", .exact("3.4.3")),
+        .package(url: "https://github.com/readdle/vapor-core.git", .upToNextMajor(from: "3.4.4")),
         
         // Event-driven network application framework for high performance protocol servers & clients, non-blocking.
-        .package(url: "https://github.com/readdle/swift-nio.git", .exact("1.9.3")),
+        .package(url: "https://github.com/readdle/swift-nio.git", .upToNextMajor(from: "1.9.4")),
 
         // Bindings to OpenSSL-compatible libraries for TLS support in SwiftNIO
-        .package(url: "https://github.com/readdle/swift-nio-ssl.git", .exact("1.2.2")),
+        .package(url: "https://github.com/readdle/swift-nio-ssl.git", .upToNextMajor(from: "1.2.3")),
     ],
     targets: [
         .target(name: "HTTP", dependencies: ["Async", "Bits", "Core", "Debugging", "NIO", "NIOOpenSSL", "NIOHTTP1"]),
